ff75037851744748bb79aebaaa5dd2cd73852782
Rutuja <rutuja.adsul31@gmail.com>
2024-03-16 03:23:27 +0530

    abhushan jwellery project

--- jwelleryproj/jwelleryapp/views.py
+++ jwelleryproj/jwelleryapp/views.py
@@ -386,24 +386,10 @@ def payment_callback(request):
         order.paymentstatus = payment['status']
         order.save()
         # send mail to user for payment success
-        send_mail('Payment Success', 'Your payment is successfull', 'rutuja.adsul31@gmail.com', [order.userid.email])
+        send_mail('Abhushan Jewellery Payment Success', 'Your payment is successfull', 'rutuja.adsul31@gmail.com', [order.userid.email])
 
         context={"order_id ":order.orderid,"amount":payment['amount'],"status":payment['status']}
         return render(request, 'paymentsuccess.html',context)
     else:
         return HttpResponse('failure')
 
-# {
-#     "id": "order_IluGWxBm9U8zJ8",
-#     "entity": "order",
-#     "amount": 5000,
-#     "amount_paid": 0,
-#     "amount_due": 5000,
-#     "currency": "INR",
-#     "receipt": "rcptid_11",
-#     "offer_id": null,
-#     "status": "created",
-#     "attempts": 0,
-#     "notes": [],
-#     "created_at": 1642662092
-# }
